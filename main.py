b990fb7dca36cba3f9d015a8deb70c1c0180c0c8
Andrew <antongyang@yahoo.com>
2022-10-29 19:30:54 -0600

    added newline

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ elif overview_choice == '2':
 else:
     sys.exit("Did not enter a valid option.")
 
-league = input('Which league?')
+league = input('Which league?\n')
 type = input('Which type?\nCurrency (Currency)\nFragment (Currency)\nArtifact (Item)\nOil (Item)\nIncubator (Item)\nScarab (Item)\nFossil (Item)\nResonator (Item)\nEssence (Item)\nDivinationCard (Item)\nSkillGem (Item)\nBaseType (Item)\nHelmetEnchant (Item)\nUniqueMap (Item)\nMap (Item)\nUniqueJewel (Item)\nUniqueFlask (Item)\nUniqueWeapon (Item)\nUniqueArmour (Item)\nUniqueAccessory (Item)\nBeast (Item)\nDeliriumOrb (Item)\nInvitation (Item)\nClusterJewel (Item)\nVial (Item)\n')
 
 def construct_url(url_base, league, type):
